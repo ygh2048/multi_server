58a45891e593d008e08d8bcb5eb35d3d257eb984
“ygh2048” <ygh2048009576@outlook.com>
2025-10-24 00:34:55 +0800

    chore: tidy task_tcp.c remove stray sample comment block

--- USER/task_tcp.c
+++ USER/task_tcp.c
@@ -33,49 +33,7 @@
  */
 
 void vMBPortTCPPool(void);
-
-
-OS_STK TCP_TASK_STK[TCP_STK_SIZE];
-OS_STK LED1_TASK_STK[LED1_STK_SIZE];
-
-
-
-#define MODBUS_PORT  502
-#define ETHERNET_BUF_MAX_SIZE (1024 * 2)
-/* network information */
-wiz_NetInfo default_net_info = {
-    {0x00, 0x08, 0xdc, 0x12, 0x22, 0x12},   /* mac */
-    {192, 168, 103, 200},                   /* ip  */
-    {255, 255, 255, 0},                     /* sn  */
-    {192, 168, 103, 1},                     /* gw  */
-    {8, 8, 8, 8},                           /* dns */
-    NETINFO_STATIC                          /* dhcp */
-};
-uint8_t ethernet_buf[ETHERNET_BUF_MAX_SIZE] = {0};
-uint16_t local_port = 502;
-
 //TCP任务
-/*
-
-
-/*
-void tcp_task(void *pdata)
-{
-    printf("%s TCP Server Multi Socket example\r\n",_WIZCHIP_ID_);
-    wizchip_reset();
-    delay_ms(10);
-    //wizchip init 
-    wizchip_initialize();
-    delay_ms(10);
-    network_init(ethernet_buf, &default_net_info);
-    delay_ms(10);
-    SPI2_SetSpeed(8);//提速
-    while(1)
-    {
-        multi_tcps_socket(ethernet_buf, local_port);
-    };
-}
-*/
 
 
 void tcp_task(void *pdata)
